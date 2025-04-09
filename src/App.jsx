89cc106738ab01216a862c90bce99259f74f0c1b
mdfarhan456 <farhanfarhan78165@gmail.com>
2025-04-09 22:36:24 +0530

    React-js

--- src/App.jsx
+++ src/App.jsx
@@ -70,16 +70,88 @@
 
 //css................................................................................
 
+// const App=()=>{
+//   return(
+//     <>
+//   <div style={{border:"5px solid red" , height:"500px" , width:"500px" , borderRadius:"50px", padding:"40px" , margin:"auto", backgroundColor:"blue"}}>
+//     <div style={{border:"5px solid red" , height:"400px" , width:"400px" , borderRadius:"50px" , padding:"40px" , backgroundColor:"pink"}}>
+//       <div style={{border:"5px solid red" , height:"300px" , width:"300px" , borderRadius:"50px" , padding:"40px" , backgroundColor:"green"}}>
+//         <h1 style={{padding:"95px"}}>Cybrom</h1>
+//       </div>
+//     </div>
+//   </div>
+//     </>
+//   )
+// }
+// export default App;
+
+
+
+
+
+//useestate................................................................
+
+
+
+// import Button from 'react-bootstrap/Button';
+// import { useState } from "react";
+
+// const App=()=>{
+//   const [ct , setct]=useState(0)
+
+//   const inc=()=>{
+
+//     setct(ct+1)
+//   }
+//   const dec=()=>{
+// if(ct<1)
+// {
+//   alert("count is less than 1")
+// }
+// else{
+//     setct(ct-1)
+// }
+//   }
+//   return(
+//     <>
+// <div style={{width:"500px" , backgroundColor:'lightgreen' ,padding:"30px", margin:"auto" , borderRadius:"20px", border:"3px solid black"}}>
+//     <h1>My counter app:</h1>
+
+//     <h1>Count:{ct}</h1>
+
+// <div >
+//     <Button variant="primary" onClick={inc} style={{marginLeft:"10px"}}>Increment</Button>
+//     <Button variant="warning" onClick={dec} style={{marginLeft:"10px"}}>Decrement</Button>
+//     <Button variant="danger" onClick={()=>{setct(0)}} style={{marginLeft:"10px"}}>Reset</Button>
+//     </div>
+//     </div>
+
+//     </>
+//   )
+
+// }
+// export default App;
+
+
+
+//useEffect......................................
+
+import { useState,useEffect } from "react";
+
+
+
 const App=()=>{
+  const [count , setcnt]=useState(0)
+  useEffect(()=>{
+    
+    setInterval(()=>{
+      setcnt(count+1);
+    },2000)
+  
+  })
   return(
     <>
-  <div style={{border:"5px solid red" , height:"500px" , width:"500px" , borderRadius:"50px", padding:"40px" , margin:"auto", backgroundColor:"blue"}}>
-    <div style={{border:"5px solid red" , height:"400px" , width:"400px" , borderRadius:"50px" , padding:"40px" , backgroundColor:"pink"}}>
-      <div style={{border:"5px solid red" , height:"300px" , width:"300px" , borderRadius:"50px" , padding:"40px" , backgroundColor:"green"}}>
-        <h1 style={{padding:"95px"}}>Cybrom</h1>
-      </div>
-    </div>
-  </div>
+    <h1>count: {count}</h1>
     </>
   )
 }
